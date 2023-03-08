98b590850577bd2819e4c39ecbbc4040ef6a021a
Carlos Andrés Pavas Correa <pavas0921@gmail.com>
2023-03-08 18:58:29 -0500

    first commit

---
+++ src/redux/reducers/messageReducer.js
@@ -0,0 +1,18 @@
+const messageReducer = (state, action) => {
+  switch (action.type) {
+    case "set":
+      return {
+        message: action.payload,
+      };
+
+    case "clear":
+      return {
+        message: "",
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default messageReducer;
